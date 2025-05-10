b69fa938e54dfe6c74aa070012a047d0348c82ba
Yasir-Zafar <yasirzafar365@gmail.com>
2025-05-11 04:24:32 +0500

    hm fix 1

--- home/flake.nix
+++ home/flake.nix
@@ -1,65 +1,75 @@
 {
   description = "Home Manager Flake with Hyprland and Cachix";
-
+  nixConfig = {
+    extra-substituters = [
+      "https://cache.nixos.org"
+      "https://hyprland.cachix.org"
+    ];
+    extra-trusted-public-keys = [
+      "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+      "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
+    ];
+  };
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-
     flake-utils.url = "github:numtide/flake-utils";
-
     hyprland = {
       url = "github:hyprwm/Hyprland";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-
-    nix-cache-action.url = "github:nix-community/nix-cache-action";
   };
-
-  outputs = { self, nixpkgs, home-manager, flake-utils, hyprland, nix-cache-action, ... }@inputs:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = import nixpkgs {
-          inherit system;
-          config.allowUnfree = true;
-        };
-
-        username = "boi"; # Replace this
-      in {
-        homeConfigurations.${username} = home-manager.lib.homeManagerConfiguration {
-          inherit pkgs;
-
+  outputs = { self, nixpkgs, home-manager, flake-utils, hyprland, ... }@inputs:
+    let
+      # Define the supported systems
+      supportedSystems = [ "x86_64-linux" "aarch64-linux" ];
+      # Get the hostname
+      hostname = "dpix720"; # Replace with your actual hostname
+      # Username 
+      username = "boi";
+      # Helper function to generate configs for each system
+      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+    in {
+      homeConfigurations = {
+        # Simple configuration: username only
+        "${username}" = home-manager.lib.homeManagerConfiguration {
+          pkgs = nixpkgs.legacyPackages.x86_64-linux;
           modules = [
             ./default.nix
-
-            ({ ... }: {
-              home.username = username;
-              home.homeDirectory = "/home/${username}";
-              home.stateVersion = "24.05";
-
-              programs.home-manager.enable = true;
-
-              nix = {
-                settings = {
-                  substituters = [
-                    "https://cache.nixos.org"
-                    "https://hyprland.cachix.org"
-                    "https://nix-community.cachix.org"
-                  ];
-                  trusted-public-keys = [
-                    "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
-                    "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
-                    "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-                  ];
-                  experimental-features = [ "nix-command" "flakes" ];
-                };
+            {
+              home = {
+                username = username;
+                homeDirectory = "/home/${username}";
+                stateVersion = "24.05";
               };
-            })
+              programs.home-manager.enable = true;
+            }
           ];
         };
+        
+        # Configuration with hostname: username@hostname
+        "${username}@${hostname}" = home-manager.lib.homeManagerConfiguration {
+          pkgs = nixpkgs.legacyPackages.x86_64-linux;
+          modules = [
+            ./default.nix
+            {
+              home = {
+                username = username;
+                homeDirectory = "/home/${username}";
+                stateVersion = "24.05";
+              };
+              programs.home-manager.enable = true;
+            }
+          ];
+        };
+      };
+      
+      # Additional packages and outputs
+      packages = forAllSystems (system: {
+        default = self.homeConfigurations."${username}@${hostname}".activationPackage;
       });
+    };
 }
-
