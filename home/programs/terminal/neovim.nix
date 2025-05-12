c67cd3dd0280139022c0c4155e784d5cd1bf2102
Yasir-Zafar <yasirzafar365@gmail.com>
2025-05-12 22:25:49 +0500

    lets try nvim again

--- home/programs/terminal/neovim.nix
+++ home/programs/terminal/neovim.nix
@@ -1,20 +1,16 @@
 { config, lib, pkgs, ... }:
 
-let
-  # Import the nixCats module
-  nixCats = import ../../../config/nvim;
-in
 {
-  # Enable nixCats Home Manager module
-  imports = [
-    nixCats.homeModule  # Import the nixCats home module
-  ];
+  programs.neovim = {
+    enable = true;
+    defaultEditor = true;
+  };
 
-  # Enable the package and module configuration in Home Manager
-  nixCats.enable = true;
-  nixCats.packageNames = [ "myHomeModuleNvim" ];  # Replace with actual package names if different
-  nixCats.luaPath = ./.;  # Set the Lua path or any other required paths
-
-  # You can also customize further by adding other options in this file
+  # Path to your existing Neovim config directory
+  home.file = {
+    ".config/nvim" = {
+      source = ../../../config/nvim;
+      recursive = true;
+    };
+  };
 }
-
